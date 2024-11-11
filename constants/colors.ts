7d167c86ae136b3f1a334085bffa336688452530
ArtsiomChults <artemorsha@mail.ru>
2024-11-11 15:02:58 +0300

    add: color const

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,5 +1,7 @@
 export const COLORS = {
     background: '#161622',
+    backgroundOpacity: 'rgba(104,103,102,0.4)',
     gray: '#CDCDE0',
+    grayOpacity: 'rgba(205,205,224,0.4)',
     buttonColor: '#FF9C01'
 }
