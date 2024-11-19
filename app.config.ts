8866f15fb975aafcc93c47fc292e860a29282708
ArtsiomChults <artemorsha@mail.ru>
2024-11-20 01:15:23 +0300

    fix: error build --6

--- app.config.ts
+++ app.config.ts
@@ -1,18 +1,19 @@
-import { ExpoConfig } from 'expo/config';
-
-const config: ExpoConfig = {
-    name: 'game',
-    slug: 'game',
-    scheme: 'your-scheme',
-    newArchEnabled: true,
-    android: {
-        package: 'com.artsiomchults.game',
-    },
-    extra: {
-        eas: {
-            projectId: '7a0da2c4-d1da-4819-ba04-daf5eedcd850',
+export default () => {
+    return {
+        expo: {
+            name: 'game',
+            slug: 'game',
+            owner: "artsiomchults",
+            scheme: 'your-scheme',
+            newArchEnabled: true,
+            android: {
+                package: 'com.artsiomchults.game',
+            },
+            extra: {
+                eas: {
+                    projectId: "7a0da2c4-d1da-4819-ba04-daf5eedcd850",
+                },
+            },
         },
-    },
+    };
 };
-
-export default config;
