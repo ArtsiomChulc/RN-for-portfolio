8406c0a0a325eda9757cca35146f9377673ce6e1
ArtsiomChults <artemorsha@mail.ru>
2024-11-25 22:00:19 +0300

    add: colors

--- constants/colors.ts
+++ constants/colors.ts
@@ -5,5 +5,8 @@ export const COLORS = {
     grayOpacity: 'rgba(205,205,224,0.4)',
     buttonColor: '#FF9C01',
     backgroundAccordion: '#0b0a0a',
-    buttonCalc: '#dfc3c3'
+    buttonCalc: '#dfc3c3',
+    white: '#ffffff',
+    black: '#000000',
+    blackOpacity: 'rgba(0,0,0,0.7)',
 }
