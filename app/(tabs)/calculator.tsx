08c2e157c457027d4985a2349db85e55ef391f34
ArtsiomChults <artemorsha@mail.ru>
2024-11-25 22:55:48 +0300

    fix: style some fix

--- app/(tabs)/calculator.tsx
+++ app/(tabs)/calculator.tsx
@@ -1,11 +1,21 @@
 import React, {useState} from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+    ScrollView,
+    StyleProp,
+    StyleSheet,
+    Text,
+    useWindowDimensions,
+    View, ViewStyle
+} from 'react-native';
 import Button from "@/components/button/Button";
 import {COLORS} from "@/constants/colors";
 
 export default function Calculator() {
     const [result, setResult] = useState<string>('');
     const [current, setCurrent] = useState<string>('');
+
+    const {width, height} = useWindowDimensions();
+    const isPortrait = height > width;
 
     const handleButtonPress = (value: string) => {
         if (value === '=') {
@@ -26,69 +36,94 @@ export default function Calculator() {
         }
     };
 
-    const renderButton = (value: string) => (
-        <Button title={value} backgroundColor={COLORS.buttonCalc}
-                widthPercent={value === 'C' ? '60%' : '22%'}
+    const buttons = [
+        '1', '2', '3', '-',
+        '4', '5', '6', '*',
+        '7', '8', '9', '/',
+        '0', '.', '=', '+',
+        'C',
+    ];
+
+    const renderButton = (value: string) => {
+        const isClearButton = value === 'C';
+        const widthPercent = isClearButton
+            ? isPortrait
+                ? '60%'
+                : '21%'
+            : isPortrait
+                ? '21%'
+                : '10%';
+
+        return (
+            <Button
+                key={value}
+                title={value}
+                backgroundColor={COLORS.buttonCalc}
+                widthPercent={widthPercent}
                 onClick={() => handleButtonPress(value)}
-        />
-    );
+            />
+        );
+    };
+
+    const renderButtons = () => {
+        if (isPortrait) {
+            // Кнопки по 4 в ряд
+            const rows = [];
+            for (let i = 0; i < buttons.length; i += 4) {
+                rows.push(
+                    <View key={`row-${i}`} style={styles.row}>
+                        {buttons.slice(i, i + 4).map(renderButton)}
+                    </View>
+                );
+            }
+            return rows;
+        } else {
+            // Кнопки по 6 в ряд
+            const rows = [];
+            for (let i = 0; i < buttons.length; i += 6) {
+                rows.push(
+                    <View key={`row-${i}`} style={styles.row}>
+                        {buttons.slice(i, i + 6).map(renderButton)}
+                    </View>
+                );
+            }
+            return rows;
+        }
+    };
+
+    const containerStyle: StyleProp<ViewStyle> = {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: COLORS.background,
+        color: COLORS.gray,
+    }
 
     return (
-        <ScrollView>
-            <View style={styles.container}>
-                <View style={styles.resultContainer}>
-                    <Text style={styles.currentText}>{current}</Text>
-                    <Text style={styles.resultText}>{result}</Text>
-                </View>
-                <View style={styles.buttonsContainer}>
-                    <View style={styles.row}>
-                        {renderButton('1')}
-                        {renderButton('2')}
-                        {renderButton('3')}
-                        {renderButton('-')}
-                    </View>
-                    <View style={styles.row}>
-                        {renderButton('4')}
-                        {renderButton('5')}
-                        {renderButton('6')}
-                        {renderButton('*')}
-                    </View>
-                    <View style={styles.row}>
-                        {renderButton('7')}
-                        {renderButton('8')}
-                        {renderButton('9')}
-                        {renderButton('/')}
-                    </View>
-                    <View style={styles.row}>
-                        {renderButton('0')}
-                        {renderButton('.')}
-                        {renderButton('=')}
-                        {renderButton('+')}
-                    </View>
-                    <View style={[styles.row, styles.clearBtn]}>
-                        {renderButton('C')}
-                    </View>
-                </View>
+        <ScrollView contentContainerStyle={[containerStyle, {flexGrow: 1}]}>
+            <View style={styles.resultContainer}>
+                <Text style={styles.currentText}>{current}</Text>
+                <Text style={styles.resultText}>{result}</Text>
+            </View>
+            <View style={styles.buttonsContainer}>
+                {renderButtons()}
+                {/*<View style={[styles.row, styles.clearBtn]}>*/}
+                {/*    <Button title={'C'} backgroundColor={COLORS.buttonCalc}*/}
+                {/*            widthPercent={isPortrait ? "60%" : "20%"}*/}
+                {/*            onClick={() => handleButtonPress('C')}*/}
+                {/*    />*/}
+                {/*</View>*/}
             </View>
         </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 10,
-        backgroundColor: COLORS.background,
-        color: COLORS.gray,
-    },
     resultContainer: {
         width: '100%',
-        flexGrow:   1,
+        paddingTop: 30,
+        paddingHorizontal: 10,
         justifyContent: 'flex-end',
         alignItems: 'flex-end',
-        padding: 20,
         borderBottomWidth: 1,
         borderColor: COLORS.gray,
     },
@@ -104,15 +139,14 @@ const styles = StyleSheet.create({
     },
     buttonsContainer: {
         alignItems: 'center',
-        flex: 2,
         padding: 10,
         marginTop: 10,
-        gap: 10
+        gap: 10,
     },
     row: {
         flexDirection: 'row',
         justifyContent: 'center',
-        gap: 10
+        gap: 10,
     },
     clearBtn: {
         alignSelf: 'flex-start',
